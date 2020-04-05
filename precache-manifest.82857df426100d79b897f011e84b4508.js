250dde8e5c4cc66c999a615b30a403cbff00eb72
vladok089 <vladok.sha089@gmail.com>
2020-04-05 20:52:03 +0200

    build app

---
+++ precache-manifest.82857df426100d79b897f011e84b4508.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4001293f6488ae2c915376cbdb94bbd8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f4272f4b23eb79b5257",
+    "url": "/static/css/3.6121d13d.chunk.css"
+  },
+  {
+    "revision": "e94dec45c8eecfb1506b",
+    "url": "/static/css/4.ff08e0c6.chunk.css"
+  },
+  {
+    "revision": "aebec585d15fdb7f1799",
+    "url": "/static/css/main.879dcd25.chunk.css"
+  },
+  {
+    "revision": "961e188a159c87053fb6",
+    "url": "/static/js/2.b2d6ecf9.chunk.js"
+  },
+  {
+    "revision": "6f4272f4b23eb79b5257",
+    "url": "/static/js/3.9dded79f.chunk.js"
+  },
+  {
+    "revision": "e94dec45c8eecfb1506b",
+    "url": "/static/js/4.a9d49654.chunk.js"
+  },
+  {
+    "revision": "aebec585d15fdb7f1799",
+    "url": "/static/js/main.8cf68423.chunk.js"
+  },
+  {
+    "revision": "f6db87d0146590183d4d",
+    "url": "/static/js/runtime~main.c2bf2480.js"
+  },
+  {
+    "revision": "ff3087a399698a2632334ed574d9828f",
+    "url": "/static/media/loader.ff3087a3.svg"
+  }
+]);
